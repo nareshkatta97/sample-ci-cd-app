c05a2b19f3642bc986feca9cf3fa843af2c3a1f0
nareshkatta97 <nareshkatta97@gmail.com>
2026-05-10 16:34:37 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('CI/CD Pipeline Working. that is cool!! 🚀');
+    res.send('CI/CD Pipeline Working. that is cool!! Working With Ashok! 🚀');
 });
 
 app.get('/health', (req, res) => {
